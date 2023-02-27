03bf424d5da16ad1d24932c4b0f8c189dc8d34f5
Emmersmacks <visnevskijan@gmail.com>
2023-02-27 18:20:43 +0500

    camera fix

--- Assets/Scripts/CameraMoveComponent.cs
+++ Assets/Scripts/CameraMoveComponent.cs
@@ -1,33 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class CameraMoveComponent : MonoBehaviour
 {
     public float Speed;
+
+    public CinemachineVirtualCamera FixedCamera;
+    public CinemachineVirtualCamera FreeLookCameraCamera;
     
     private Camera _camera;
+    private CinemachineBrain _cameraBrain;
     private Transform _target;
 
     private Transform _currentPosition;
+    private ECameraMode _cameraMode;
 
     void Start()
     {
         _camera = Camera.main;
+        _cameraBrain = _camera.GetComponent<CinemachineBrain>();
     }
     
     void Update()
     {
-        if (_target != null)
+        if (_cameraMode == ECameraMode.Fixed)
         {
-            var targetPosition = _target.position + new Vector3(-0.2f, 0, 0.5f);
-            var direction = targetPosition - _camera.transform.position;
-            _camera.transform.position += direction.normalized * Speed * Time.deltaTime;
-            if (Vector3.Distance(targetPosition, _camera.transform.position) < 0.01)
+            if (_target != null)
             {
-                _currentPosition = _target;
-                _target = null;
+                var targetPosition = _target.position + new Vector3(-0.2f, 0, 0.5f);
+                var direction = targetPosition - _camera.transform.position;
+                _camera.transform.position += direction.normalized * Speed * Time.deltaTime;
+                if (Vector3.Distance(targetPosition, _camera.transform.position) < 0.01)
+                {
+                    _currentPosition = _target;
+                    _target = null;
+                }
             }
+        }
+        else
+        {
+            
         }
     }
 
@@ -39,4 +53,26 @@ public class CameraMoveComponent : MonoBehaviour
             _target = target;
         }
     }
+
+    public void SetCameraMode(ECameraMode mode)
+    {
+        _cameraMode = mode;
+
+        if (mode == ECameraMode.Fixed)
+        {
+            FixedCamera.Priority = 2;
+            FreeLookCameraCamera.Priority = 1;
+        }
+        else
+        {
+            FixedCamera.Priority = 1;
+            FreeLookCameraCamera.Priority = 2;
+        }
+    }
+}
+
+public enum ECameraMode
+{
+    Fixed,
+    Observable
 }
